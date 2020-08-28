b85c9c02e7fd25cad75b62943438c2006795058a
William Roger <williamroger.frontend@gmail.com>
2020-08-27 21:28:48 -0300

    Finalizado componentes Tweet e Feed

--- src/components/Tweet/styles.ts
+++ src/components/Tweet/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { Chat, Rocketseat, Favorite } from '../../styles/Icons';
 
@@ -85,33 +85,97 @@ export const Header = styled.div`
 `;
 
 export const Dot = styled.div`
-
+  background: var(--gray);
+  width: 2px;
+  height: 2px;
+  margin: 0 10px;
 `;
 
-export const Description = styled.div`
-
+export const Description = styled.p`
+  font-size: 14px;
+  margin-top: 4px;
 `;
 
 export const ImageContent = styled.div`
+  margin-top: 12px;
+  width: 100%;
+  height: min(285px, max(175px, 41vw));
 
+  background: var(--outline);
+  border-radius: 14px;
+  
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.7;
+  }
 `;
 
 export const Icons = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
 
+  margin: 11px auto 0;
+  width: 100%; /** Mobile */
+
+  @media (min-width:330px) {
+    width: 63%;
+  }
+
+  > div {
+    cursor: pointer;
+
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+`;
+
+const iconCSS = css`
+  width: 19px;
+  height: 19px;
 `;
 
 export const Status = styled.div`
+  display: flex;
+  align-items: center;
 
+  font-size: 14px;
+
+  > svg {
+    margin-right: 5px;
+  }
+
+  &:nth-child(1) {
+    &,
+    > svg path {
+      color: var(--gray);
+    }
+  }
+  &:nth-child(2) {
+    color: var(--retweet);
+    > svg path {
+      fill: var(--retweet);
+    }
+  }
+  &:nth-child(3) {
+    color: var(--like);
+    > svg {
+      fill: var(--like);
+    }
+  } 
 `;
 
-export const CommentIcon = styled.div`
-
+export const CommentIcon = styled(Chat)`
+  ${iconCSS}
 `;
 
-export const RetweeetIcon = styled.div`
-
+export const RetweeetIcon = styled(Rocketseat)`
+  ${iconCSS}
 `;
 
-export const LikeIcon = styled.div`
-
+export const LikeIcon = styled(Favorite)`
+  ${iconCSS}
 `;
