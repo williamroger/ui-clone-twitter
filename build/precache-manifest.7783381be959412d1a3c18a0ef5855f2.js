329e5701a97c96ac1af5aa669677729094ebbe81
William Roger <williamroger.frontend@gmail.com>
2020-08-29 09:02:28 -0300

    Building App

---
+++ build/precache-manifest.7783381be959412d1a3c18a0ef5855f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5458e33ba099196e9e7f31d2e8ee22f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dccf869f557232f96635",
+    "url": "/static/js/2.1f528d53.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1f528d53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a865cca24ae02dfebe",
+    "url": "/static/js/main.19b56a14.chunk.js"
+  },
+  {
+    "revision": "ab2fb683e3945113d966",
+    "url": "/static/js/runtime-main.8da74bc3.js"
+  }
+]);
